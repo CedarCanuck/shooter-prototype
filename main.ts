812f79e868afe9eeaf14d03faff34aed183036c2
Justin Freemantle <50429451+mcxfluffy@users.noreply.github.com>
2020-09-25 10:51:09 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
 namespace SpriteKind {
     export const Rotate = SpriteKind.create()
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, bedi, 50, 50)
+    if (goose.overlapsWith(projectile)) {
+        goose.destroy()
+        projectile.destroy()
+        music.baDing.play()
+    }
+})
+info.onLifeZero(function () {
+    game.showLongText("You Died!", DialogLayout.Bottom)
+})
+sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
+    info.changeScoreBy(10)
+})
+let projectile: Sprite = null
+let goose: Sprite = null
+let bedi: Sprite = null
 tiles.setTilemap(tilemap`level_0`)
-let mySprite = sprites.create(img`
+bedi = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . f f f f f f f f f f . . . 
     . . . f f f e e e e f f f . . . 
@@ -20,16 +54,39 @@ let mySprite = sprites.create(img`
     . . b b b f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Rotate)
-transformSprites.rotateSprite(mySprite, 0)
+transformSprites.rotateSprite(bedi, 0)
 let positiveRotation = 10
 let negativeRotation = -10
+info.setLife(3)
+goose = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)
+goose.follow(bedi)
+if (bedi.overlapsWith(goose)) {
+    info.changeLifeBy(-1)
+}
 forever(function () {
     if (controller.left.isPressed()) {
-        transformSprites.changeRotation(mySprite, negativeRotation)
+        transformSprites.changeRotation(bedi, negativeRotation)
         pause(15)
     }
     if (controller.right.isPressed()) {
-        transformSprites.changeRotation(mySprite, positiveRotation)
+        transformSprites.changeRotation(bedi, positiveRotation)
         pause(15)
     }
 })
